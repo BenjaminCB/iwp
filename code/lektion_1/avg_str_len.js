13af1172ecc1b97bcc8b8097fafdb8e89b1a546d
Ace_BCB <benjamincb@hotmail.dk>
2021-02-03 22:01:34 +0100

    improved avg_str_len.js from lektion 1

--- code/lektion_1/avg_str_len.js
+++ code/lektion_1/avg_str_len.js
@@ -9,9 +9,5 @@ console.log(avgStrLen(array));
 console.log(avgStrLen(strings));
 
 function avgStrLen(arr) {
-    let sum = 0;
-    for (i of arr) {
-        sum += i.length;
-    };
-    return sum /= arr.length;
+    return arr.reduce((a, b) => a + b).length / arr.length;
 }
