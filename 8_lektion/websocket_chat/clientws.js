1f07928674d9f71a770e02bc5f87bf10c629630b
Ace_BCB <benjamincb@hotmail.dk>
2021-04-10 15:48:40 +0200

    made the websockt chat

--- 8_lektion/websocket_chat/clientws.js
+++
@@ -1,9 +0,0 @@
-let exampleSocket = new WebSocket('ws://127.0.0.1:3000/hello');
-
-exampleSocket.onopen = function (event) {
-    exampleSocket.send("Here's some text that the server is urgently awaiting!");
-};
-
-exampleSocket.onmessage = function (event) {
-    console.log(event.data);
-}
